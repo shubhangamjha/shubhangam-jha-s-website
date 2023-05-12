31e1e43d5b03c1873455691731141bd059d7659d
shubhangamjha <105533871+shubhangamjha@users.noreply.github.com>
2023-05-12 08:32:52 +0000

    Dynamic data and styling web page

--- main.py
+++ main.py
@@ -2,10 +2,39 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+JOBS = [
+  {
+    'id': '1',
+    'title': 'Data Analysist',
+    'location': 'Bengaluru, India',
+    'salary': '₹10,00,000'
+    
+  },
+
+  {
+    'id': '2',
+    'title': 'Data Scientist',
+    'location': 'Greater, India',
+    'salary': '₹15,00,000'
+    
+  },
+
+  {
+    'id': '3',
+    'title': 'Data Engineer',
+    'location': 'California, USA',
+    
+    
+  }
+]
+
 @app.route("/")
 
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', 
+                         jobs=JOBS,
+                        company_name= 'Terminal Stacks')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True) 
